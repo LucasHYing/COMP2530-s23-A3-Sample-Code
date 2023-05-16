522e11b5f6a53d1ab1dc946c4827fabddbe81992
Lucas <113465142+LucasHYing@users.noreply.github.com>
2023-05-15 21:44:22 -0700

    commit

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const PAGE_SIZE = 10
 let currentPage = 1;
 let pokemons = []
+const numPagesBtn = 5;
 
 
 const updatePaginationDiv = (currentPage, numPages) => {
   $('#pagination').empty()
 
-  const startPage = 1;
-  const endPage = numPages;
-  for (let i = startPage; i <= endPage; i++) {
+  const startPage = Math.max(1, currentPage-Math.floor(numPagesBtn/2));
+  const endPage = Math.min(numPages, currentPage+Math.floor(numPagesBtn/2));
+
+
+  if (currentPage > 1) {
     $('#pagination').append(`
-    <button class="btn btn-primary page ml-1 numberedButtons" value="${i}">${i}</button>
+    <button type="button" class="btn btn-primary page ml-1 numberedButtons" value="${currentPage - 1}">Prev</button>
+    `)
+  }
+
+
+  for (let i = startPage; i <= endPage; i++) {
+    var active = "";
+    if (i == currentPage) {
+      active = "active";
+    }
+
+    $('#pagination').append(`
+    <button type="button" class="btn btn-primary page ml-1 ${active} numberedButtons" value="${i}">${i}</button>
+    `)
+  }
+
+  if (currentPage < numPages) {
+    $('#pagination').append(`
+    <button type="button" class="btn btn-primary page ml-1 numberedButtons" value="${currentPage + 1}">Next</button>
     `)
   }
 
@@ -29,10 +50,53 @@ const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
           More
         </button>
-        </div>  
-        `)
+      </div>  
+    `)
   })
+
+  const totalPokemons = pokemons.length;
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = Math.min(startIndex + PAGE_SIZE, totalPokemons);
+  const displayedPokemons = endIndex - startIndex;
+
+  $('#displayedPokemons').text(`Showing ${displayedPokemons} of ${totalPokemons} pokemons`);
 }
+
+
+const setupFilters = async () => {
+  // Add event listener to type filters
+  $('.typeFilter').change((e) => {
+    const selectedTypes = $('.typeFilter:checked').map((_, checkbox) => checkbox.value).get();
+
+    // Filter the Pokémon based on the selected type(s)
+    if (selectedTypes.length === 0) {
+      // No type selected, show all Pokémon
+      currentPage = 1;
+      paginate(currentPage, PAGE_SIZE, pokemons);
+      const numPages = Math.ceil(pokemons.length / PAGE_SIZE);
+      updatePaginationDiv(currentPage, numPages);
+      const totalPokemons = pokemons.length;
+      $('#displayedPokemons').text(`Showing ${Math.min(totalPokemons, PAGE_SIZE)} of ${totalPokemons} pokemons`);
+    } else {
+      // Filter by the selected type(s)
+      const filteredPokemons = pokemons.filter((pokemon) => {
+        const pokemonTypes = pokemon.types.map((type) => type.type.name);
+        return selectedTypes.some((type) => pokemonTypes.includes(type));
+      });
+      currentPage = 1;
+      paginate(currentPage, PAGE_SIZE, filteredPokemons);
+      const numPages = Math.ceil(filteredPokemons.length / PAGE_SIZE);
+      updatePaginationDiv(currentPage, numPages);
+      const totalPokemons = filteredPokemons.length;
+      $('#displayedPokemons').text(`Showing ${Math.min(totalPokemons, PAGE_SIZE)} of ${totalPokemons} pokemons`);
+    }
+  });
+};
+
+
+
+
+
 
 const setup = async () => {
   // test out poke api using axios here
@@ -42,6 +106,15 @@ const setup = async () => {
   let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
   pokemons = response.data.results;
 
+  setupFilters();
+
+
+  const totalPokemons = pokemons.length;
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = Math.min(startIndex + PAGE_SIZE, totalPokemons);
+  const displayedPokemons = endIndex - startIndex;
+
+  $('#displayedPokemons').text(`Showing ${displayedPokemons} of ${totalPokemons} pokemons`);
 
   paginate(currentPage, PAGE_SIZE, pokemons)
   const numPages = Math.ceil(pokemons.length / PAGE_SIZE)
